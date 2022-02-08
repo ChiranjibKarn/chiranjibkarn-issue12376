b7a139a2dcb6ce90ce48529c35f00ce24198bcf1
ChiranjibKarn <chiranjib.karn@gmail.com>
2022-02-08 15:08:19 +0545

    issue 12367 replicated

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,36 +1,130 @@
-import grails.util.BuildSettings
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.rolling.RollingFileAppender
 import grails.util.Environment
-import org.springframework.boot.logging.logback.ColorConverter
-import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
 
-import java.nio.charset.StandardCharsets
+def commonPattern = '%d{dd.MM.yyyy HH:mm:ss.SSS} [%thread] %-5level %logger{35} - %msg%n'
 
-conversionRule 'clr', ColorConverter
-conversionRule 'wex', WhitespaceThrowableProxyConverter
-
+// See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        charset = StandardCharsets.UTF_8
-
-        pattern =
-                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
-                        '%clr(%5p) ' + // Log level
-                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
-                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
-                        '%m%n%wex' // Message
+        pattern = commonPattern
     }
 }
 
-def targetDir = BuildSettings.TARGET_DIR
-if (Environment.isDevelopmentMode() && targetDir != null) {
-    appender("FULL_STACKTRACE", FileAppender) {
-        file = "${targetDir}/stacktrace.log"
+if (Environment.current == Environment.DEVELOPMENT) {
+    root(INFO, ['STDOUT'])
+} else {
+    appender('ROLLING', RollingFileAppender) {
+        file = '/var/log/sparkwork/backend.log'
         append = true
         encoder(PatternLayoutEncoder) {
-            charset = StandardCharsets.UTF_8
-            pattern = "%level %logger - %msg%n"
+            pattern = commonPattern
+        }
+        rollingPolicy(TimeBasedRollingPolicy) {
+            FileNamePattern = '/var/log/sparkwork/backend-%d.log.zip'
         }
     }
-    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+
+    appender('PERFORMANCE', RollingFileAppender) {
+        file = '/var/log/sparkwork/performance.log'
+        append = true
+        encoder(PatternLayoutEncoder) {
+            pattern = commonPattern
+        }
+        rollingPolicy(TimeBasedRollingPolicy) {
+            FileNamePattern = '/var/log/sparkwork/performance-%d.log.zip'
+        }
+    }
+    appender('FRONTEND', RollingFileAppender) {
+        file = '/var/log/sparkwork/frontend.log'
+        append = true
+        encoder(PatternLayoutEncoder) {
+            pattern = commonPattern
+        }
+        rollingPolicy(TimeBasedRollingPolicy) {
+            FileNamePattern = '/var/log/sparkwork/frontend-%d.log.zip'
+        }
+    }
+
+    appender('MOBILE', RollingFileAppender) {
+        file = '/var/log/sparkwork/mobile.log'
+        append = true
+        encoder(PatternLayoutEncoder) {
+            pattern = commonPattern
+        }
+        rollingPolicy(TimeBasedRollingPolicy) {
+            FileNamePattern = '/var/log/sparkwork/mobile-%d.log.zip'
+        }
+    }
+
+    appender('SQS', RollingFileAppender) {
+        file = '/var/log/sparkwork/sqs.log'
+        append = true
+        encoder(PatternLayoutEncoder) {
+            pattern = commonPattern
+        }
+        rollingPolicy(TimeBasedRollingPolicy) {
+            FileNamePattern = '/var/log/sparkwork/sqs-%d.log.zip'
+        }
+    }
+
+    appender('SEARCH', RollingFileAppender) {
+        file = '/var/log/sparkwork/search.log'
+        append = true
+        encoder(PatternLayoutEncoder) {
+            pattern = commonPattern
+        }
+        rollingPolicy(TimeBasedRollingPolicy) {
+            FileNamePattern = '/var/log/sparkwork/search-%d.log.zip'
+        }
+    }
+
+    appender('SSO', RollingFileAppender) {
+        file = '/var/log/sparkwork/sso.log'
+        append = true
+        encoder(PatternLayoutEncoder) {
+            pattern = commonPattern
+        }
+        rollingPolicy(TimeBasedRollingPolicy) {
+            FileNamePattern = '/var/log/sparkwork/sso-%d.log.zip'
+        }
+    }
+
+    appender('RESTNOTIFICATION', RollingFileAppender) {
+        file = '/var/log/sparkwork/restnotification.log'
+        append = true
+        encoder(PatternLayoutEncoder) {
+            pattern = commonPattern
+        }
+        rollingPolicy(TimeBasedRollingPolicy) {
+            FileNamePattern = '/var/log/sparkwork/restnotification-%d.log.zip'
+        }
+    }
+
+    appender('VAPI', RollingFileAppender) {
+        file = '/var/log/sparkwork/vapi.log'
+        append = true
+        encoder(PatternLayoutEncoder) {
+            pattern = commonPattern
+        }
+        rollingPolicy(TimeBasedRollingPolicy) {
+            FileNamePattern = '/var/log/sparkwork/vapi-%d.log.zip'
+        }
+    }
+
+    logger('PERFORMANCE', DEBUG, ['PERFORMANCE'])
+    logger('FRONTEND', ERROR, ['FRONTEND'], false)
+    logger('MOBILE', ERROR, ['MOBILE'], false)
+    logger('SQS', INFO, ['SQS'])
+    logger('SEARCH', INFO, ['SEARCH'])
+    logger('SSO', INFO, ['SSO'])
+    logger('VAPI', DEBUG, ['VAPI'], false)
+    logger('RESTNOTIFICATION', ERROR, ['RESTNOTIFICATION'])
+    root(WARN, ['ROLLING'])
 }
-root(ERROR, ['STDOUT'])
+/*Suppressed noisy package from user agent parser lib*/
+logger('nl.basjes.parse.useragent.utils', ERROR)
+logger('nl.basjes.parse.useragent', ERROR)
+//logger('ws.schild.jave.FFMPEGExecutor', DEBUG)
+//logger('ws.schild.jave.Encoder', DEBUG)
--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,22 @@
+import io.sparkwork.common.CustomUserDetailsService
+import io.sparkwork.json.JsonRenderer
+import org.springframework.web.servlet.i18n.SessionLocaleResolver
+import socket.config.WebSecurityConfig
+import socket.config.WebSocketConfig
+import socket.config.WebSocketLogInfo
+import socket.event.StompConnectEvent
+
 // Place your Spring DSL code here
 beans = {
+    webSocketConfig WebSocketConfig
+    webSecurityConfig WebSecurityConfig
+    stompConnectEvent StompConnectEvent
+    webSocketLogInfo WebSocketLogInfo
+    localeResolver(SessionLocaleResolver) {
+        defaultLocale= new Locale('en-US');
+    }
+
+    corsFilter(CorsFilter)
+    accessTokenJsonRenderer(JsonRenderer)
+    userDetailsService(CustomUserDetailsService)
 }
